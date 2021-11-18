7335e7ec0d27e443f9f76dc56465db84362db1fb
fetttttjoe <45294596+fetttttjoe@users.noreply.github.com>
2021-11-18 16:00:41 +0100

    Update BubbleSort_visualisation.py

--- BubbleSort_visualisation.py
+++ BubbleSort_visualisation.py
@@ -1,6 +1,8 @@
+import time
 import pygame
-import numpy
+import numpy as np
 from screeninfo import get_monitors
+ARRAY_LENGTH = 10000
 def Visualize():
     win.fill((0,0,0))
     # Draw lines
@@ -13,25 +15,25 @@ def BubbleSort():
     IsSwapped = True
     while(IsSwapped):
         IsSwapped = False
+        
         for i in range(len(values[0]) - iterations - 1):
-            if (values[0][i] > values[0][i+1]):
-                values[0][i], values[0][i+1] = values[0][i+1], values[0][i] 
+            if (values[0][i] > values[0][i + 1]):
+                values[0][i], values[0][i + 1] = values[0][i + 1], values[0][i] 
                 IsSwapped = True
                 swaps +=1
         Visualize()
         iterations += 1
     return iterations, swaps
-
-
 if __name__ == '__main__':
     for monitor in get_monitors():
         monitor=str(monitor).split(',')
+    #make sure we get the correct monitor size
     monitor = monitor[2].split('='), monitor[3].split('=')
-    print(monitor)
+    
     # Set parameters
     win_width   = int(int(monitor[0][1])/2) 
     win_height  = int(int(monitor[1][1])/2)
-    values = numpy.random.rand(1,win_width)*win_height 
+    values = np.random.rand(1, win_width)*win_height 
     
     # Initialize pygame
     pygame.init()
@@ -44,10 +46,16 @@ if __name__ == '__main__':
         pygame.time.delay(100)
     
         if (done == False):
+            t0 = time.time()
             iterations, swaps = BubbleSort()
+            t1 = time.time()
             done = True
-            print(f'\nNumber of swaps made: {swaps} in {iterations} iterations')
+            print(f'\nMade {swaps} swaps in {iterations} iterations\n')
+            print(f"The whole Process took {t1-t0} seconds")
         # Make sure we can close the window with 'X'
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 stop = True
+
+
+
